fe9d032d127cf18cd73925cbf5274d4adcb0d2f4
adrian_ortuzar <adrianortuzar@gmail.com>
2013-11-25 21:39:22 +0100

    media queries for different size, desktop, tablet,movie

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+'use strict';
 
+(function(window){
+	var body = $('body');
+	var win = $(window);
+	var isMovile = false;
+	var wasMovile = false;
+	
+	var row = body.find('.row');
+	
+	if(isMovileSize()){
+		printLayoutMovil();
+		
+	}else{
+		isMovile = false;
+	}
+
+	//resize event
+	win.resize(function(){
+		var isMovile = isMovileSize();
+
+		if( isMovile != wasMovile && isMovile ){
+			printLayoutMovil();
+		}
+		else if(isMovile != wasMovile && !isMovile){
+			printLayoutDesktop();
+		}
+
+		wasMovile = isMovile;
+	});
+
+	function isMovileSize(){
+		if(win.width() < 767){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function printLayoutMovil(){
+		var colright = body.find('.colright');
+		colright.remove();
+		row.prepend(colright);
+	}
+	function printLayoutDesktop(){
+		var colleft = body.find('.colleft');
+		colleft.remove();
+		row.prepend(colleft);
+	}
+	
+})(this);
